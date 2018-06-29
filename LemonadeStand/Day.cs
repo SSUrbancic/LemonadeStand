f61fe6c32437ebae710e6d4909e63bac1635190d
Sam <SSUrbancic@outlook.com>
2018-06-29 15:36:02 -0500

    In the day class: created a method to generate a certain number of customers per day that is influenced by the weather. Also made a method to make a pitcher of lemonade. Pitchers of lemonade are made when a customer makes a purchase and the cupcount is equal to zero. There are 11 cups of lemonade per pitcher. The willBuyLemonade bool is generated and dictated by a probablity statement from the customer class. If willBuyLemonade == true lemons and sugar per pitcher(dictated by playeOne's recipe) will be subtracted from playerOne's inventory. Also, with every purchase made iceCubes per pitcher will be stubrtacted from inventory. And Price per cup will be added to playerOne's money. In order to accomplish this playerOne, weather, and customer classes had to be passed into the day class. Still need to set a loop to account for multiple days of lemonade sales.

--- LemonadeStand/Day.cs
+++ LemonadeStand/Day.cs
@@ -9,11 +9,55 @@ namespace LemonadeStand
     class Day
     {
         //MEMBERS(HAS A)
+        public int customerCountPerDay;
+        public int totalLemonadesSold;
+        public double moneyMadePerDay;
+        public int cupCount = 0;
+        Player playerOne;
+        Weather weather;
+        Customer customer;
+
 
 
         //CONSTRUCTOR
-
+        public Day(Player playerOne, Weather weather, Customer customer)
+        {
+            this.playerOne = playerOne;
+            this.weather = weather;
+            this.customer = customer;
+        }
         //METHODS
-
+        public void GenerateCustomersPerDay()
+        {
+            int baseNumberOfCustomersPerDay = 99;
+            customerCountPerDay = baseNumberOfCustomersPerDay + weather.weatherInfluence;
+        }
+        public void MakePitcherOfLemonade()
+        {
+            if (playerOne.lemonadeRecipe.lemonsPerPitcher > playerOne.lemonCount || playerOne.lemonadeRecipe.sugarPerPitcher > playerOne.sugarCount)
+            {
+                cupCount = 0;
+            }
+            else
+            {
+                playerOne.lemonCount -= playerOne.lemonadeRecipe.lemonsPerPitcher;
+                playerOne.sugarCount -= playerOne.lemonadeRecipe.sugarPerPitcher;
+                cupCount = 11;
+            }
+        }
+        public void CustomerPurchases()
+        {
+            if (cupCount == 0)
+            {
+                MakePitcherOfLemonade();
+                if (customer.willBuyLemonade == true)
+                {
+                    playerOne.cupCount -= 1;
+                    playerOne.iceCubeCount -= playerOne.lemonadeRecipe.iceCubesPerCup;
+                    playerOne.playerMoney += playerOne.lemonadeRecipe.pricePerCup;
+                    cupCount -= 1;
+                }
+            }
+        }
     }
 }
